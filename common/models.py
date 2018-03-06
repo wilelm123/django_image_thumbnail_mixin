92d0ef0f4be8c1d683b7f6f39b8ca8e7423127f2
Lei Chen <lei.chen@teletraan.io>
2018-03-06 09:47:26 +0800

    fix: update models
    update models file

--- common/models.py
+++ common/models.py
@@ -1,10 +1,14 @@
 import os
 import io
 import hashlib
+import logging
 
 from PIL import Image
-from django.db import models
+from django.core.cache import cache
 from django.core.files.uploadedfile import SimpleUploadedFile
+
+
+logger = logging.getLogger(__name__)
 
 
 class ImageThumbnailMixin:
@@ -13,12 +17,17 @@ class ImageThumbnailMixin:
 
     for example:
 
-    class User(models.Model):
+    class User(models.Model, ImageThumbnailMixin):
         name = models.CharField(max_length=100)
         avatar = models.ImageField(upload_to=settings.UPLOAD_TO, null=True, blank=True)
         thumbnail = models.Image(upload_to=settings.UPLOAD_TO, null=True, blank=True)
 
-        IMAGE_FIELD, THUMBNAIL_FIELD, BASE_SIZE = "avatar", "thumbnail", 200
+        IMAGE_FIELD, THUMBNAIL_FIELD, BASE_SIZE = "avatar", "thumbnail", 100
+
+        def save(self, *args, **kwargs):
+            res = super().save(*args, **kwargs)
+            self.create_thumbnail()     # should put after super calling
+            return res
 
     """
 
@@ -27,17 +36,16 @@ class ImageThumbnailMixin:
     def __init__(self, *args, **kwargs):
         assert self.IMAGE_FIELD is not None, "ImageThumbnailMixin should set IMAGE_FIELD first"
         assert self.THUMBNAIL_FIELD is not None, "ImageThumbnailMixin should set THUMBNAIL_FIELD first"
+        self.key_prefix = "IMAGE_CACHE_{0}".format(self.__class__.__name__)
         super(ImageThumbnailMixin, self).__init__(*args, **kwargs)
 
-        self.cache_old_image_md5()
-
     def cache_old_image_md5(self):
-        if getattr(self, self.IMAGE_FIELD):
-            image_md5 = self.get_image_md5(self.IMAGE_FIELD)
-            setattr(self, "md5_{0}_cache".format(self.IMAGE_FIELD), image_md5)
+        image_md5 = self.get_image_md5()
+        key = "{0}_{1}".format(self.key_prefix, self.id)
+        cache.set(key, image_md5)
 
-    def get_image_md5(self, image_field):
-        image = getattr(self, image_field)
+    def get_image_md5(self):
+        image = getattr(self, self.IMAGE_FIELD)
         if not image or not image._file:
             return None
         md5 = hashlib.md5()
@@ -45,19 +53,29 @@ class ImageThumbnailMixin:
             md5.update(chunk)
         val = md5.hexdigest()
         image.seek(0)
+        logger.debug("Image md5 val: %s", val)
         return val
 
     def image_changed(self):
-        cached_md5 = getattr(self, "md5_{0}_cache".format(self.IMAGE_FIELD), None)
-        return cached_md5 != self.get_image_md5(self.IMAGE_FIELD)
+        key = "{0}_{1}".format(self.key_prefix, self.id)
+        cached_md5 = cache.get(key)
+        new_md5 = self.get_image_md5()
+        logger.debug("Cached Image Md5: %s", cached_md5)
+        logger.debug("New Image Md5: %s", new_md5)
+        if cached_md5 != new_md5:
+            self.cache_old_image_md5()
+        return cached_md5 != new_md5
 
-    def is_image(self, field):
-        f = io.BytesIO(field.read())
+    def get_image_format(self):
         try:
-            Image.open(f)
-            return True
-        except OSError:
-            return False
+            image = getattr(self, self.IMAGE_FIELD)
+            f = io.BytesIO(image.read())
+            img = Image.open(f)
+            image.seek(0)
+            return img.format
+        except OSError as exc:
+            logger.error("Get image file format error: %s", exc)
+            return None
 
     def create_thumbnail(self):
         """
@@ -67,17 +85,17 @@ class ImageThumbnailMixin:
         image_field = getattr(self, self.IMAGE_FIELD)
         thumbnail_field = getattr(self, self.THUMBNAIL_FIELD)
 
-        if not isinstance(image_field, models.ImageField):
-            return
-
+        logger.debug("===== Now check the image validation: %s", image_field.name)
+        img_format = self.get_image_format()
         # image field not set or file is not image or image not changed
-        if not image_field or not image_field._file or \
-                not hasattr(image_field.file, "content_type") or \
-                not self.is_image(image_field) or \
-                not self.image_changed():
+        if not image_field or not img_format or not self.image_changed():
+            logger.error("===== Skip create thumbnail")
             return
+        logger.debug("Done")
 
-        django_type = image_field.file.content_type
+        logger.debug("====== Now Create thumbnail for the given image: %s", image_field.name)
+
+        django_type = "image/{0}".format(img_format.lower())
         pil_type, file_extension = "jpeg", 'jpg'
 
         if django_type == "image/png":
@@ -95,3 +113,5 @@ class ImageThumbnailMixin:
         suf = SimpleUploadedFile(file_name, temp_handler.read(),
                                  content_type=django_type)
         thumbnail_field.save(file_name, suf, save=False)
+
+        logger.debug("Done")
